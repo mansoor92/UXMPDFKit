b417e62e2ff9e4dd1fd005d666f4ecfe3a7cf394
Mansoor Ali <mali@panteragroup.org>
2020-04-21 13:36:27 +0500

    removed font from library

--- Example/UXMPDFKit/ExampleViewController.swift
+++ Example/UXMPDFKit/ExampleViewController.swift
@@ -14,8 +14,14 @@ class ExampleViewController: UIViewController {
     @IBAction func loadPDF() {
 
         let url = Bundle.main.path(forResource: "sample2", ofType: "pdf")!
-        let document = try! UXMPDFDocument.from(filePath: url, fontSize: 20)
-        
+
+		let bundle = Bundle.main
+		UIFont.registerFont(withFilenameString: "nationalPro-Bold.ttf", bundle: bundle)
+		UIFont.registerFont(withFilenameString: "nationalPro-Regular.ttf", bundle: bundle)
+		UXMPDFDocument.font = UIFont(name: "MyriadPro-Bold", size: 16)
+
+        let document = try! UXMPDFDocument.from(filePath: url)
+
         let pdf = UXMPDFViewController(document: document!)
         pdf.annotationController.annotationTypes = [
             PDFHighlighterAnnotation.self,
@@ -26,6 +32,38 @@ class ExampleViewController: UIViewController {
         
         self.navigationController?.pushViewController(pdf, animated: true)
     }
+}
+
+extension UIFont {
+
+	static func registerFont(withFilenameString filenameString: String, bundle: Bundle) {
+
+		guard let pathForResourceString = bundle.path(forResource: filenameString, ofType: nil) else {
+			print("UIFont+:  Failed to register font - path for resource not found.")
+			return
+		}
+
+		guard let fontData = NSData(contentsOfFile: pathForResourceString) else {
+			print("UIFont+:  Failed to register font - font data could not be loaded.")
+			return
+		}
+
+		guard let dataProvider = CGDataProvider(data: fontData) else {
+			print("UIFont+:  Failed to register font - data provider could not be loaded.")
+			return
+		}
+
+		guard let font = CGFont(dataProvider) else {
+			print("UIFont+:  Failed to register font - font could not be loaded.")
+			return
+		}
+
+		var errorRef: Unmanaged<CFError>? = nil
+		if (CTFontManagerRegisterGraphicsFont(font, &errorRef) == false) {
+			print("UIFont+:  Failed to register font - register graphics font failed - this font may have already been registered in the main bundle.")
+		}
+	}
+
 }
 
 
--- Pod/Classes/Renderer/UXMPDFDocument.swift
+++ Pod/Classes/Renderer/UXMPDFDocument.swift
@@ -38,11 +38,7 @@ open class UXMPDFDocument: NSObject, NSCoding {
     open var annotationsStore: UXMAnnotationStore = UXMAnnotationStore()
 	public static var font: UIFont?
     
-	public static func from(filePath: String, password: String? = nil, fontSize: CGFloat) throws -> UXMPDFDocument? {
-		let bundle = Bundle(for: UXMPDFDocument.self)
-		UIFont.registerFont(withFilenameString: "nationalPro-Bold.ttf", bundle: bundle)
-		UIFont.registerFont(withFilenameString: "nationalPro-Regular.ttf", bundle: bundle)
-		UXMPDFDocument.font = UIFont(name: "MyriadPro-Bold", size: fontSize)
+	public static func from(filePath: String, password: String? = nil) throws -> UXMPDFDocument? {
         if let document = try UXMPDFDocument.unarchiveDocument(filePath: filePath, password: password) {
             return document
         }
@@ -290,34 +286,3 @@ open class UXMPDFDocument: NSObject, NSCoding {
     }
 }
 
- extension UIFont {
-
-	static func registerFont(withFilenameString filenameString: String, bundle: Bundle) {
-
-		guard let pathForResourceString = bundle.path(forResource: filenameString, ofType: nil) else {
-			print("UIFont+:  Failed to register font - path for resource not found.")
-			return
-		}
-
-		guard let fontData = NSData(contentsOfFile: pathForResourceString) else {
-			print("UIFont+:  Failed to register font - font data could not be loaded.")
-			return
-		}
-
-		guard let dataProvider = CGDataProvider(data: fontData) else {
-			print("UIFont+:  Failed to register font - data provider could not be loaded.")
-			return
-		}
-
-		guard let font = CGFont(dataProvider) else {
-			print("UIFont+:  Failed to register font - font could not be loaded.")
-			return
-		}
-
-		var errorRef: Unmanaged<CFError>? = nil
-		if (CTFontManagerRegisterGraphicsFont(font, &errorRef) == false) {
-			print("UIFont+:  Failed to register font - register graphics font failed - this font may have already been registered in the main bundle.")
-		}
-	}
-
-}
